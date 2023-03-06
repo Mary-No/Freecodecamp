df96b8f28cc338abd14e8578482d2821023b221d
Novik_Maria <novik.work.mail@gmail.com>
2023-03-06 17:23:30 +0300

    task Basic Algorithm Scripting - 6

---
+++ BasicAlgorithmScripting_06.js
@@ -0,0 +1,8 @@
+function confirmEnding(str, target) {
+    if (str.substr(-(target.length), target.length) === target) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  confirmEnding("Bastian", "n");
